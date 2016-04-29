afab1a7a85aa3a64f894e2a22e41e3b731d2364c
Ishan Agrawal <ishan.agrawal97@gmail.com>
2016-04-28 23:01:02 -0700

    Added Display

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include "neural-network/NeuralNetwork.h"
-#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 using namespace std;
+using namespace cv;
 
 int main(){
+  CvCapture * capture;
+  Mat frame;
+  capture = cvCaptureFromCAM(-1);
+  namedWindow("Display Window", WINDOW_AUTOSIZE);
+  if(capture){
+    while(true){
+      frame = cvarrToMat(cvQueryFrame(capture));
+      flip(frame, frame, 1);
+      imshow("Display Window", frame);
+
+      int c = waitKey(10);
+      if((char)c == 'c') {
+        break;
+      }
+    }
+  }
 
   return 0;
 }
